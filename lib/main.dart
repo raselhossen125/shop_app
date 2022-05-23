d190aee589259503d76695a3019d5c2f7edf18d2
rasel-hossen <straselhossen24@gmail.com>
2022-05-23 23:05:52 +0600

    productOverView demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, deprecated_member_use
+
 import 'package:flutter/material.dart';
+
+import 'screens/products_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,29 +10,31 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
+    Map<int, Color> pokeballRedSwatch = {
+      50: Color.fromRGBO(238, 21, 21, 0.1),
+      100: Color.fromRGBO(238, 21, 21, 0.2),
+      200: Color.fromRGBO(238, 21, 21, 0.3),
+      300: Color.fromRGBO(238, 21, 21, 0.4),
+      400: Color.fromRGBO(238, 21, 21, 0.5),
+      500: Color.fromRGBO(238, 21, 21, 0.6),
+      600: Color.fromRGBO(238, 21, 21, 0.7),
+      700: Color.fromRGBO(238, 21, 21, 0.8),
+      800: Color.fromRGBO(238, 21, 21, 0.9),
+      900: Color.fromRGBO(238, 21, 21, 1.0),
+    };
+    MaterialColor pokeballRed = MaterialColor(0xff293462, pokeballRedSwatch);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'MyShop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: pokeballRed,
+        accentColor: Colors.deepOrange,
+        fontFamily: "Lato"
       ),
-      home: MyHomePage(),
+      home: ProductsOverviewScreen(),
     );
   }
 }
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("MyShop"),
-      ),
-    );
-  }
-}
-
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors, sort_child_properties_last, prefer_const_constructors, deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Color.fromARGB(221, 15, 15, 15),
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
